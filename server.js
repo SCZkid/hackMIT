1a105cee152656f4e2f0776fb1a56eaf3b24409a
Collin Stedman <cstedman@princeton>
2013-10-05 17:40:40 -0400

    Holy text analysis, Batman

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-var http = require("http");
-var url = require("url");
-var express = require("express");
+// Builtin packages
+var http = require('http');
+var url = require('url');
+
+// AlchemyAPI
+var alchemyapi = require('./alchemyapi');
+
+// Express
+var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb:hack:hack,hack.kalosal.com/wheee');
 var db = mongoose.connection;
-db.once('open', function callback () {
+/*db.once('open', function callback () {
     console.log("hooray!");
     var jfaoiwejfoiawe = mongoose.model('jfaoiwejfoiawe', { firstName: String });
     var jordan = new jfaoiwejfoiawe({
@@ -29,14 +35,24 @@ db.once('open', function callback () {
     })
     //var Jordan = new Person({ firstName: 'Jordan', lastName: 'Haines', age: 20});
 });
+*/
 
+app.set('port', process.env.PORT || 3001);
+app.use(express.favicon());
+app.use(express.bodyParser());
+//app.use(express.methodOverride());
 
+//Check port
+var port = process.env.PORT || 3001;
+app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
 
 function start(route) {
 	app.use(express.static(__dirname + '/Public'));
 	app.get('/', function(request, response) {
 		var pathname = url.parse(request.url).pathname;
-		console.log("Request for " + pathname + " received.");
+		console.log('Request for ' + pathname + ' received.');
         route(pathname);
         response.sendfile('Public/testPage.html')
 		//response.end();
@@ -49,7 +65,27 @@ function start(route) {
 		//response.end();
 	});
 	app.listen(9001);
-	console.log("Server has started.");
+	console.log('Server has started.');
 }
 
+app.get('/example', example);
+
 exports.start = start;
+
+// Replace this with the files we read!
+var demo_text = 'My physics professor says that Newton discovered F=m*a, where m is mass and a is acceleration. F is force, measured in Newtons, an SI unit.';
+
+// What is this?
+function example(req, res) {
+  var output = {};
+
+  // Start the analysis chain
+  concepts(req, res, output);
+}
+
+function concepts(req, res, output) {
+  alchemyapi.concepts('text', demo_text, { 'showSourceText':1 }, function(error, response) {
+    output['concepts'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response['concepts'] };
+    res.send(output);
+  });
+}
