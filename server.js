64a8793f3ab1feabd67e87c1330cfef6e0452781
Steven Zierk <stevenz@mit.edu>
2013-10-05 14:02:49 -0400

    somewhat working server + page

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var http = require("http");
 var url = require("url");
+var express = require("express");
+var app = express();
 
 function start(route) {
-	function onRequest(request, response) {
+	app.get('/', function(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-
-		route(pathname);
-
-		response.writeHead(200, {"Content-Type": "text/html"});
-		response.write("<h1>" + pathname + "</h1>");
-		response.end();
-	}
-	http.createServer(onRequest).listen(80);
+        route(pathname);
+        response.sendfile('Pages/testPage.html')
+		//response.end();
+	});
+	app.listen(82);
 	console.log("Server has started.");
 }
 
