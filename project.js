49beef056934bc8459b7d4fc137119cdebec8ccf
Collin Stedman <cstedman@princeton>
2013-10-06 02:10:08 -0400

    Added the revision function to kick off alchemy

--- project.js
+++ project.js
@@ -5,49 +5,58 @@ var router = require("./router");
 var alchemyapi = require('./alchemyapi');
 
 server.start(router.route);
-var app = server.app;
-app.get('/example', example);
+//var app = server.app;
+//app.get('/example', example);
 
-var demo_text = 'My physics professor says that Newton discovered F=m*a, where m is mass and a is acceleration. F is force, measured in Newtons, an SI unit.';
+var revision = function (contents) {
+  var output = {};
+  concepts(output, contents);
+}
 
-function example(req, res) {
+//var contents = 'My physics professor says that Newton discovered F=m*a, where m is mass and a is acceleration. F is force, measured in Newtons, an SI unit.';
+
+/* function example(req, res) {
   var output = {};
 
   concepts(req, res, output);
 }
+*/
 
-function concepts(req, res, output) {
-  alchemyapi.concepts('text', demo_text, { 'showSourceText':0 }, function(error, response) {
-    output['concepts'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response['concepts'] };
-    keywords(req, res, output);
+function concepts(output, contents) {
+  alchemyapi.concepts('text', contents, { 'showSourceText':0 }, function(error, response) {
+    output['concepts'] = { text:contents, response:JSON.stringify(response,null,4), results:response['concepts'] };
+    keywords(output, contents);
   });
 }
 
-function keywords(req, res, output) {
-  alchemyapi.keywords('text', demo_text, {}, function(error, response) {
-    output['keywords'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response['keywords'] };
-    category(req, res, output);
+function keywords(output, contents) {
+  alchemyapi.keywords('text', contents, {}, function(error, response) {
+    output['keywords'] = { text:contents, response:JSON.stringify(response,null,4), results:response['keywords'] };
+    category(output, contents);
   });
 }
 
-function category(req, res, output) {
-  alchemyapi.category('text', demo_text, {}, function(error, response) {
-    output['relations'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response };
-    relations(req, res, output);
+function category(output, contents) {
+  alchemyapi.category('text', contents, {}, function(error, response) {
+    output['relations'] = { text:contents, response:JSON.stringify(response,null,4), results:response };
+    relations(output, contents);
   });
 }
 
-function relations(req, res, output) {
-  alchemyapi.relations('text', demo_text, {}, function(error, response) {
-    output['relations'] = {text:demo_text, response:JSON.stringify(response,null,4), results:response['relations'] };
-    entities(req, res, output);
+function relations(output, contents) {
+  alchemyapi.relations('text', contents, {}, function(error, response) {
+    output['relations'] = {text:contents, response:JSON.stringify(response,null,4), results:response['relations'] };
+    entities(output, contents);
   });
 }
 
-function entities(req, res, output) {
-  alchemyapi.entities('text', demo_text, { 'quotations':1 }, function(error, response) {
-    output['entities'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response['entities'] };
-    res.send(output);
+function entities(output, contents) {
+  alchemyapi.entities('text', contents, { 'quotations':1 }, function(error, response) {
+    output['entities'] = { text:contents, response:JSON.stringify(response,null,4), results:response['entities'] };
+    // THIS HAS TO BE REPLACED WITH AN INSERT INTO THE DATABASE!!!!!
+    console.log(output, contents);
   });
 }
 
+// A test for the revision function
+//revision("This is a really bad test about Newton and cookies and milk and the moon.");
